f57f320375c8ff33f56e73a48c087f060f1aa93b
Steve Wang <stevew77@gmail.com>
2026-05-16 01:39:03 +0800

    feat: update AutoBreadcrumbs to use DB query, remove db0/unstorage deps
    AutoBreadcrumbs now uses getBreadcrumbsQuery instead of COURSES import.
    Removed db0 and unstorage from package.json. Cleaned up storage.ts.

--- src/components/AutoBreadcrumbs.tsx
+++ src/components/AutoBreadcrumbs.tsx
@@ -1,44 +1,23 @@
-import { useParams } from "@solidjs/router";
+import { createAsync, useParams } from "@solidjs/router";
 import { createMemo } from "solid-js";
 import Breadcrumbs from "~/components/Breadcrumbs";
-import { COURSES, SITE_NAME } from "~/utils/constants";
+import { getBreadcrumbsQuery } from "~/server/quest-store";
+import { SITE_NAME } from "~/utils/constants";
 
 export default function AutoBreadcrumbs() {
   const params = useParams();
 
+  const dbCrumbs = createAsync(() =>
+    getBreadcrumbsQuery(
+      params.course as string,
+      params.category as string | undefined,
+      params.subsection as string | undefined,
+    ),
+  );
+
   const items = createMemo(() => {
-    const crumbs: { label?: string; href?: string }[] = [
-      { label: SITE_NAME, href: "/" },
-    ];
-
-    if (!params.course) return crumbs;
-
-    const course = COURSES[params.course as string];
-    if (course) {
-      crumbs.push({ label: course.title, href: `/${params.course}` });
-
-      const category = course.categories.find(
-        (c) => c.category === params.category,
-      );
-      if (category) {
-        crumbs.push({
-          label: category.title,
-          href: `/${params.course}/${params.category}`,
-        });
-
-        const subsection = category.subsections.find(
-          (s) => s.subsection === params.subsection,
-        );
-        if (subsection) {
-          crumbs.push({
-            label: subsection.title,
-            href: `/${params.course}/${params.category}/${params.subsection}`,
-          });
-        }
-      }
-    }
-
-    return crumbs;
+    const crumbs = dbCrumbs() ?? [];
+    return [{ label: SITE_NAME, href: "/" }, ...crumbs];
   });
 
   return <Breadcrumbs items={items()} />;
