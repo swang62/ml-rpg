427c658cf4a2e147e79be3df3c18bd0e617ed561
Steve Wang <stevew77@gmail.com>
2026-05-19 10:16:05 +0800

    feat: add ensureCourseDb startup check with error if DB not found

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,10 +1,12 @@
 import Database from "better-sqlite3";
+import { ensureCourseDb } from "~/server/startup";
 import { COURSE_DB_PATH } from "~/utils/constants";
 
 let _db: Database.Database | null = null;
 
 export function getDb(): Database.Database {
   if (!_db) {
+    ensureCourseDb();
     _db = new Database(COURSE_DB_PATH);
     _db.pragma("journal_mode = WAL");
   }
