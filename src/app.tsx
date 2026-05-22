34a8b28e4dcd2f507220368b6d6887b3b724c448
Steve Wang <stevew77@gmail.com>
2026-05-22 16:51:32 +0800

    feat: add seo

--- src/app.tsx
+++ src/app.tsx
@@ -3,23 +3,23 @@ import "./legacy-shim.css";
 import "@fontsource-variable/plus-jakarta-sans";
 import "@fontsource/press-start-2p";
 
-import { Link, MetaProvider, Title } from "@solidjs/meta";
+import { MetaProvider } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import AuthProvider from "~/components/AuthContext";
 import ErrorBoundary from "~/components/ErrorBoundary";
 import ParallaxBackground from "~/components/ParallaxBackground";
+import SEO from "~/components/SEO";
 import { GlobalBackspaceHandler, KeyboardNavHandler } from "~/utils/keyboard";
-import { SITE_NAME } from "./utils/constants";
 
 export default function App() {
   return (
     <Router
       root={(props) => (
         <MetaProvider>
-          <Title>{SITE_NAME}</Title>
-          <Link rel="icon" href="/favicon.ico" />
+          <SEO />
+
           <AuthProvider>
             <GlobalBackspaceHandler />
             <KeyboardNavHandler />
